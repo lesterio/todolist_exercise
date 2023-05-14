dd7f57549d1098c8924470597643d6e7da430ab3
lester <lesterli1989@gamil.com>
2023-05-14 22:33:46 +0800

    httpClient的可怕東西!

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,7 @@
+import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: []
+  providers: [ provideHttpClient()]
+  //Todo：幹，這個真的是會忘記… 要使用HttpClient的功能，記得要在這裡import providers哈哈哈哈
 };
--- src/todolistbody/todolistbody.component.ts
+++ src/todolistbody/todolistbody.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component , inject ,OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { Todoitem } from 'src/todoitem';
 import { FormsModule } from '@angular/forms';
+//import { HttpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-todolistbody',
@@ -12,16 +13,35 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./todolistbody.component.css']
 })
 export class TodolistbodyComponent {
-  itemDesc = ''
-  todoitems : Todoitem[] = [
-    { itemDesc : '睡覺' , isCompelete : false},
-    { itemDesc : '吃早餐' , isCompelete : true},
-    { itemDesc : '吃午餐' , isCompelete : false},
-    { itemDesc : '吃晚餐' , isCompelete : false},
-    { itemDesc : '吃宵夜' , isCompelete : false},
-  ]
 
-  clear = () => this.todoitems = []
+  itemDesc = ''
+  currentItem : Todoitem = { itemDesc : '' , isCompelete : false}
+  // todoitems : Todoitem[] = [
+  //   { itemDesc : '睡覺' , isCompelete : false},
+  //   { itemDesc : '吃早餐' , isCompelete : true},
+  //   { itemDesc : '吃午餐' , isCompelete : false},
+  //   { itemDesc : '吃晚餐' , isCompelete : false},
+  //   { itemDesc : '吃宵夜' , isCompelete : false},
+  // ]
+  todoitems : Todoitem[] = [];
+
+
+
+  //request = inject(HttpClient) //定義一個請求
+  httpClient = inject(HttpClient);
+  // ngOnInit(): void {
+  //   this.request.get<Todoitem[]>('http://localhost:4200/assets/data/todoitems.json')
+  //   .subscribe((data) => { this.todoitems = data })
+  // }
+
+
+
+  clearItem = () => this.todoitems = []
+  readDefaultTodoItem(){
+    //Todo:用httpGet試試
+    //this.request.get<Todoitem[]>('http://localhost:4200/assets/data/todoitems.json').subscribe(
+    //(data) => { this.todoitems = data })
+  }
   addItem(){
     this.itemDesc !== '' ?
       this.todoitems.push({itemDesc : this.itemDesc , isCompelete : false}):''
@@ -29,7 +49,7 @@ export class TodolistbodyComponent {
   }
 
   removeItem(data : Todoitem){
-    console.log(data)
+    //console.log(data)
     return this.todoitems = this.todoitems.filter( item => item !== data)
   }
 
