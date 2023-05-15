9e2ebe0da70d5f6c1a95bdd44e282be650b7b71b
lester <lesterli1989@gamil.com>
2023-05-15 14:13:12 +0800

    發現了一個連動上的bug，調整寫法

--- src/todolistbody/todolistbody.component.ts
+++ src/todolistbody/todolistbody.component.ts
@@ -1,5 +1,5 @@
 import { Component , inject ,OnInit} from '@angular/core';
-import { CommonModule} from '@angular/common';
+import { CommonModule, NgSwitch} from '@angular/common';
 import { Todoitem } from 'src/todoitem';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
@@ -14,63 +14,109 @@ import { HttpClient } from '@angular/common/http';
 })
 export class TodolistbodyComponent implements OnInit {
   checkMessage = ''
-  checkValue? :boolean
   inputDesc = ''
   maxSeq : number = 0
   currentItem? : Todoitem //= { id : 0 , itemDesc : '' , isCompelete : false}
   //? 表示，可允許空值
-  todoitems : Todoitem[] = [];
+  displayMode : string = 'All'
+  todoItems : Todoitem[] = [];
+
   request = inject(HttpClient);//定義一個請求
   ngOnInit(){
     this.request.get<Todoitem[]>('http://localhost:4200/assets/data/todoitems.json').subscribe(
     (response) => {
-      this.todoitems = response
+      this.todoItems = response
+      //this.todoItems = response
       //暫且做個timesCount
-      this.todoitems.forEach(item=>{
+      this.todoItems.forEach(item=>{
         this.maxSeq = item.id > this.maxSeq ? item.id : this.maxSeq
       })
     })
   }
-  clearItem = () => this.todoitems = []
+  clearItem = () => this.todoItems = []
   readDefaultTodoItem(){
     //Todo:用httpGet試試
     this.request.get<Todoitem[]>('http://localhost:4200/assets/data/todoitems.json').subscribe(
     (response) => {
-      this.todoitems = response
+      this.todoItems = response
       //暫且做個timesCount
-      this.todoitems.forEach(item=>{
+      this.todoItems.forEach(item=>{
         this.maxSeq = item.id > this.maxSeq ? item.id : this.maxSeq
       })
     })
+    this.todoItems = this.todoItems
   }
   clearCheckMessage = () => this.checkMessage = ''
   addItem(){
     this.maxSeq += 1
     if(this.inputDesc!=='')
-      this.todoitems.push({ id : this.maxSeq , itemDesc : this.inputDesc , isCompelete : false})
+      this.todoItems.push({ id : this.maxSeq , itemDesc : this.inputDesc , isCompelete : false})
     else
       this.checkMessage = '待辦事項不得為空'
     this.inputDesc = ''
   }
   removeItem(data : Todoitem){
-    return this.todoitems = this.todoitems.filter( item => item !== data)
+    return this.todoItems = this.todoItems.filter( item => item !== data)
   }
 
   updateItemState(id :number , state : boolean){
-    let index = this.todoitems.findIndex(item=>item.id===id)
-    if(index > -1) this.todoitems[index].isCompelete = state
+    let index = this.todoItems.findIndex(item=>item.id===id)
+    if(index > -1) this.todoItems[index].isCompelete = state
   }
   updateItemDescription(id : number , description : string){
-    let index = this.todoitems.findIndex(item => { item.id === id  })
-    if(index !== null) this.todoitems[index].itemDesc = description
+    let index = this.todoItems.findIndex(item => { item.id === id  })
+    if(index !== null) this.todoItems[index].itemDesc = description
   }
 
   setItemState(state:boolean){
-    this.todoitems.forEach(item=>item.isCompelete = state)
+    this.todoItems.forEach(item=>item.isCompelete = state)
   }
 
   //不確定怎樣的寫法比較好，再研究XD
-  getItemByStatusCount = (compelete : boolean) => { return this.todoitems.filter(t => t.isCompelete===compelete).length; }
-  getCompeleteItemCount = () => { return this.todoitems.filter(t => t.isCompelete===true).length; }
-  getProcessingItemCount = () => { return this.todoitems.filter(t => t.isCompelete===false).length; }
+  getItemByStateCount = (compelete : boolean) => {
+     return this.todoItems.filter(t => t.isCompelete===compelete).length; }
+  getCompeleteItemCount = () => { return this.todoItems.filter(t => t.isCompelete===true).length; }
+  getProcessingItemCount = () => { return this.todoItems.filter(t => t.isCompelete===false).length; }
+
+  getItems = () => this.todoItems;
+  getCompelteItems() {
+    let current : Todoitem[] = [];
+    current = this.todoItems.filter(t => t.isCompelete=== true);
+    return current;
+  }
+  getProcessingItems() {
+    let current : Todoitem[] = [];
+    current = this.todoItems.filter(t => t.isCompelete=== false);
+    return current;
+  }
+
+  //先留著這種想法，但在這次好像不太適合。
+  //好像也可以用enum?
+   getItemsByState(state : string |'All'|'Active'|'Completed'){
+    //console.log(state)
+    let current :Todoitem[] = [];
+     switch(state){
+      case 'All':
+        current = this.todoItems;
+        break;
+       case 'Active':
+        current = this.todoItems.filter(t => t.isCompelete=== false);
+        break;
+       case 'Completed':
+        current = this.todoItems.filter(t => t.isCompelete===true);
+        break;
+       default:
+        current = this.todoItems
+     }
+     return current;
+
+
+   }
+
+
+  //todo : 什麼叫pure function?還是感覺不太出來，待研究。
+  //目前的理解是，input是什麼，相對應的output就應該固定的
+  //可是，在寫程式的時候，常常就是會用到共用變數不是嗎？  那這樣不就會變影響到了？
+  // eg : getItemByStateCount(boolean) ，但實際應該要把整個array丟進來？ getItemByStateCount(Todoitems[],boolea)?
+  //這樣感覺是肥大耶(?!)
 }
